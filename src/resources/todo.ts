669c7c73728009696468a466014c149466bc842a
jamal moosvi <jimishah514@gmail.com>
2021-03-28 18:40:19 +0500

    updated resouces/demo, routes/common

--- src/resources/todo.ts
+++ src/resources/todo.ts
@@ -4,13 +4,13 @@ export class TodoApi {
     static async postTodo(obj: any) {
         try {
             console.log("obj : ",obj);
-            // const coupon = await todo.create({  id: obj.id,
-            //     name: obj.name,
-            //     description: obj.description, 
+            // const todo = await todo.create({  id: obj.id,
+            //     id: obj.id,
+            //     title: obj.title, 
             //     status: obj.status, 
             //     }
             // );
-            // console.log("coupon : ",coupon);
+            // console.log("todo : ",todo);
         } catch(e) {
             throw Error(e.message);
         }
@@ -19,8 +19,8 @@ export class TodoApi {
     static async getTodos() {
         try {
             
-            // const coupon = await todo.todos.list();
-            // return coupon;
+            // const todo = await todo.todos.list();
+            // return todo;
         } catch {
             throw Error("Error Occured while getting todo list");
             return -1;
